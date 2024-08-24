ca497f2d3a9cdb9c55ff341bf9d7791c727b5d34
Nidhay Pancholi <52347410+NidhayPancholi@users.noreply.github.com>
2024-08-24 21:00:00 +0000

    Added count of numeric and symbol cols

--- extend.py
+++ extend.py
@@ -2,6 +2,7 @@
 #   python3.13 -B extend.py ../moot/optimize/[comp]*/*.csv
 
 import sys,random
+import ezr
 from ezr import the, DATA, csv, dot
 
 def show(lst):
@@ -11,11 +12,19 @@ def myfun(train):
   d    = DATA().adds(csv(train))
   x    = len(d.cols.x)
   size = len(d.rows)
+  numeric = 0
+  symbols = 0
+  for col in d.cols.x:
+    if isinstance(col,ezr.NUM):
+      numeric +=1
+    else:
+      symbols+=1
+  
   dim  = "small" if x <= 5 else ("med" if x < 12 else "hi")
   size = "small" if size< 500 else ("med" if size<5000 else "hi")
-  return [dim, size, x,len(d.cols.y), len(d.rows), train[17:]]
+  return [dim, size, x,len(d.cols.y), len(d.rows),numeric, symbols ,train[17:]]
 
 random.seed(the.seed) #  not needed here, but good practice to always take care of seeds
-show(["dim", "size","xcols","ycols","rows","file"])
-show(["------"] * 6)
+show(["dim", "size","xcols","ycols","rows","numcols","symcols","file"])
+show(["------"] * 9)
 [show(myfun(arg)) for arg in sys.argv if arg[-4:] == ".csv"]
